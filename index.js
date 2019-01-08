702018b52ac80689e5a89f4a5a8e1d47fc095068
Elizabeth Chan <lchan217@gmail.com>
2019-01-08 23:09:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ function destructivelyAppendKitten(name) {
   return kittens.push("Ralph")
 }
 
+function destructivelyPrependKitten(name) {
+  return kittens.unshift("Broom")
+}
+
 function destructivelyRemoveLastKitten() {
   return kittens.pop()
 }
