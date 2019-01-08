7b46ba9e1cb4aabbc40bcd2ba04f098810672b16
Elizabeth Chan <lchan217@gmail.com>
2019-01-08 23:12:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,3 +22,9 @@ function destructivelyRemoveFirstKitten() {
   function prependKitten(name) {
     return [name, ...kittens]
   }
+  
+  function removeLastKitten(){
+    return (kittens.slice(0, kittens.length-1))
+  }
+  
+  
